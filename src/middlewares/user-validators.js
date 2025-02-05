2f0abe553c1c510a7fdec5efb0910b54ff687132
mrodriguez-2023292 <mrodriguez-2023292@kinal.edu.gt>
2025-02-05 15:27:04 -0600

    se hicieron cambios en el codigo

--- src/middlewares/user-validators.js
+++ src/middlewares/user-validators.js
@@ -26,7 +26,7 @@ export const registerValidator = [
 export const loginValidator = [
     body("email").optional().isEmail().withMessage("No es un email válido"),
     body("username").optional().isString().withMessage("Username es en formáto erróneo"),
-    body("password").isLength({min: 4}).withMessage("El password debe contener al menos 8 caracteres"),
+    body("password").isLength({min: 8}).withMessage("El password debe contener al menos 8 caracteres"),
     validarCampos,
     handleErrors
 ]
